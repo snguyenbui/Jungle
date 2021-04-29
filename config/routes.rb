e3a7c8a0dd0fa3a992b3b0dc05bef208084e331a
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-29 19:05:57 +0000

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
-  resources :user, only: [:new, :create]
+  resources :users, except: [:edit, :update, :show]
 
   resource :cart, only: [:show] do
     post   :add_item

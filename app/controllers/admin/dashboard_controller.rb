249cedec716628d31b81e907078f0526a7b5e6b7
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-29 00:49:06 +0000

    added dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
   def show
+    @products = Product.all
+    @category = Category.all
   end
 end

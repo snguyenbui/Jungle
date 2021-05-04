ae2e11262df8f685f31c3f67997ab4cc07b7a7ce
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-05-04 02:12:17 +0000

    added user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, format: { with: /\A[^\W]+@[a-z0-9]+\.[a-z]{2,}\z/i } #regex \A => start of string, [^\W] => anything except non-word character, @[a-z0-9] => @ followed by alphanumirics, \.[a-z]{2,} => . followed by at least 2 letters, \z => end of string, /i => case insensitive option
-  validates :password_digest, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[^\W]+@[a-z0-9]+\.[a-z]{2,}\z/i } #regex \A => start of string, [^\W] => anything except non-word character, @[a-z0-9] => @ followed by alphanumirics, \.[a-z]{2,} => . followed by at least 2 letters, \z => end of string, /i => case insensitive option
+  validates :password, presence: true, :confirmation =>true, length: { minimum: 8 } 
+  validates_confirmation_of :password
+
+  def self.authenticate_with_credentials(email, password)
+    if user = User.find_by(email: email.downcase.strip)&.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
